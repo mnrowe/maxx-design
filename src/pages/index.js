a17a723cfe1b91315e8fcacfd60e359726e79d8a
Matthew Rowe <mrowe.w@gmail.com>
2019-12-09 22:16:35 -0500

    fix margins and maxx logo to right width

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ export default () => (
   <div class="container">
     <header>
       <img class="max-logo" src={logo} alt="MAXX Potential Logo" />
-      <quote>
+      <quote class="designer-quote">
         <h2 class="intro">HELLO I'M SAM</h2>
         <h3 class="title">Web Designer</h3>
       </quote>
     </header>
-    <img class="jumbotron" src={hero} />
+    <img class="jumbotron" src={hero} alt="Mountains with snow and big blue lake"/>
   </div>
 )
