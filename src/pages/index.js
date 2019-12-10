f85f7c799e1cf4bb3ccfd05c096afa6dcb943b68
Matthew Rowe <mrowe.w@gmail.com>
2019-12-09 21:27:49 -0500

    seperate styles to own file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import logo from "./company-logo.png"
 
 export default () => (
-  <header style={{ display: `flex` }}>
-    <img style={{ maxWidth: `150px` }} src={logo} alt="MAXX Potential Logo" />
+  <header>
+    <img src={logo} alt="MAXX Potential Logo" />
     <quote>
       <h2>HELLO I'M SAM</h2>
       <h3>Web Designer</h3>
