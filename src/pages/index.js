4b9e8e95ab12655f4d49afd4c7ac7806fb62895d
Matthew Rowe <mrowe.w@gmail.com>
2019-12-09 21:01:35 -0500

    create header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,11 @@ import React from "react"
 import logo from "./company-logo.png"
 
 export default () => (
-  <div>
-    <img src={logo} alt="MAXX Potential Logo"/>
-  </div>
+  <header>
+    <img style={{ maxWidth: `150px` }} src={logo} alt="MAXX Potential Logo" />
+    <div class="">
+      <h1>HELLO I'M SAM</h1>
+      <h2>Web Designer</h2>
+    </div>
+  </header>
 )
