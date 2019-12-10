48dc0657aed90092749e3c7ed92b058339fc9e88
Matthew Rowe <mrowe.w@gmail.com>
2019-12-09 21:47:37 -0500

    add jumbotron style image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import logo from "./company-logo.png"
+import hero from "./hero1.jpg"
 
 export default () => (
-  <header>
-    <img src={logo} alt="MAXX Potential Logo" />
-    <quote>
-      <h2>HELLO I'M SAM</h2>
-      <h3>Web Designer</h3>
-    </quote>
-  </header>
+  <div class="container">
+    <header>
+      <img class="max-logo" src={logo} alt="MAXX Potential Logo" />
+      <quote>
+        <h2 class="intro">HELLO I'M SAM</h2>
+        <h3 class="title">Web Designer</h3>
+      </quote>
+    </header>
+    <img class="jumbotron" src={hero} />
+  </div>
 )
