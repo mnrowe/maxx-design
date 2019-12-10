44bd5f2d81b9939d5b56040d8559690cd5d671fd
Matthew Rowe <mrowe.w@gmail.com>
2019-12-10 16:45:36 -0500

    fix images names and add copy right symbol to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,5 +2,5 @@ import React from "react"
 import containerStyles from "./footer.module.css"
 
 export default () => (
-    <footer className={containerStyles.container}>2016 Sam's Web Dev All Rights Reserved.</footer>
+    <footer className={containerStyles.container}>© 2016 Sam's Web Dev All Rights Reserved.</footer>
 )
