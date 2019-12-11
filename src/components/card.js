32c774b8bd74f6bb13fcff23400dfa775e02bf49
Matthew Rowe <mrowe.w@gmail.com>
2019-12-10 22:43:03 -0500

    fix url font on cards

--- src/components/card.js
+++ src/components/card.js
@@ -5,10 +5,12 @@ export default props => (
   <div className={styles.container}>
     <div className={styles.textContainer}>
       <p className={styles.title}>{props.siteName}</p>
-      <p className={styles.url}><em>{props.siteUrl}</em></p>
+      <p className={styles.url}>
+        <em>{props.siteUrl}</em>
+      </p>
     </div>
     <div className={styles.frameContainer}>
-        <img alt={props.alt} className={styles.image} src={props.src} />
+      <img alt={props.alt} className={styles.image} src={props.src} />
     </div>
   </div>
 )
