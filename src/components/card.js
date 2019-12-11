3e057392e18bb7ad4ecfb6eb18e3c43ea3309295
Matthew Rowe <mrowe.w@gmail.com>
2019-12-10 20:46:17 -0500

    fix cards

--- src/components/card.js
+++ src/components/card.js
@@ -4,8 +4,8 @@ import styles from "./card.module.css"
 export default props => (
   <div className={styles.container}>
     <div className={styles.textContainer}>
-      <h1 className={styles.title}>TECHNOLINE</h1>
-      <h2 className={styles.url}>technoline.com</h2>
+      <p className={styles.title}>TECHNOLINE</p>
+      <p className={styles.url}><em>technoline.com</em></p>
     </div>
     <img alt={props.alt} className={styles.image} src={props.src} srcset={`${props.src}-256 256w`}/>
   </div>
